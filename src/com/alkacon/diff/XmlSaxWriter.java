8fab88cab02afee3a43ad4585e88cc261a1e73ed
mMoossen <m.moossen@alkacon.com>
2005-10-21 13:34:51 +0000

    empty div tags are now closed for html

--- src/com/alkacon/diff/XmlSaxWriter.java
+++ src/com/alkacon/diff/XmlSaxWriter.java
@@ -1,7 +1,7 @@
 /*
  * File   : $Source: /alkacon/cvs/AlkaconDiff/src/com/alkacon/diff/XmlSaxWriter.java,v $
- * Date   : $Date: 2005/10/20 07:32:38 $
- * Version: $Revision: 1.1 $
+ * Date   : $Date: 2005/10/21 13:34:51 $
+ * Version: $Revision: 1.2 $
  *
  * This library is part of OpenCms -
  * the Open Source Content Mananagement System
@@ -51,7 +51,7 @@ import org.xml.sax.helpers.DefaultHandler;
  *
  * @author Alexander Kandzior 
  * 
- * @version $Revision: 1.1 $ 
+ * @version $Revision: 1.2 $ 
  * 
  * @since 6.0.0 
  */
@@ -130,6 +130,12 @@ public class XmlSaxWriter extends DefaultHandler implements LexicalHandler {
                 case '"':
                     result.append("&quot;");
                     break;
+//                case ' ':
+//                    result.append("&nbsp;");
+//                    break;
+//                case '\t':
+//                    result.append("&nbsp;&nbsp;&nbsp;&nbsp;");
+//                    break;
                 default:
                     result.append(ch);
             }
@@ -217,7 +223,9 @@ public class XmlSaxWriter extends DefaultHandler implements LexicalHandler {
 
         String elementName = resolveName(localName, qualifiedName);
         if (m_openElement) {
-            write("/>");
+            write("></");
+            write(elementName);
+            write(">");
         } else {
             if (!elementName.equals(m_lastElementName)) {
                 writeNewLine();
